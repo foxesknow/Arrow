8219a835cbc38dc65fb74db59195eb92669b3476
foxesknow <seanbatten58@gmail.com>
2015-03-10 22:46:04 +0000

    Added socket stuff

---
+++ CSharp/Source/Arrow/Arrow/Net/FixedHeaderSocketProcessor.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Arrow.Execution;
+
+namespace Arrow.Net
+{
+	public abstract class FixedHeaderSocketProcessor<THeader,TBody> : SocketProcessor, IDisposable
+	{
+		private Socket m_Socket;
+		private NetworkStream m_Stream;
+
+		private long m_Disposed;		
+
+		public FixedHeaderSocketProcessor(Socket socket)
+		{
+			if(socket==null) throw new ArgumentNullException("socket");
+
+			m_Socket=socket;
+			m_Stream=new NetworkStream(socket);
+		}
+
+		protected abstract int HeaderSize{get;}
+
+		protected abstract THeader CreateHeader(byte[] buffer);
+		protected abstract TBody CreateBody(THeader header, byte[] buffer);
+		protected abstract int GetBodySize(THeader header);
+
+		public void Start(Action<SocketProcessor,SocketProcessorResult<THeader,TBody>> handler)
+		{
+			Read(handler);
+		}
+
+		private void Read(Action<SocketProcessor,SocketProcessorResult<THeader,TBody>> handler)
+		{
+			while(this.KeepReading)
+			{
+				State state=new State();
+				state.Handler=handler;
+
+				int headerSize=this.HeaderSize;
+				state.HeaderBuffer=new byte[headerSize];
+				state.HeaderOffset=0;
+
+				BeginReadHeader(state);
+			}
+		}
+
+		private void BeginReadHeader(State state)
+		{
+			HandleNetworkCall(()=>
+			{
+				m_Stream.BeginRead(state.HeaderBuffer,state.HeaderOffset,state.HeaderBuffer.Length-state.HeaderOffset,ar=>EndReadHeader(ar,state),null);
+			});
+		}
+
+		private void EndReadHeader(IAsyncResult result, State state)
+		{
+			HandleNetworkCall(()=>
+			{
+				int bytesRead=m_Stream.EndRead(result);
+
+				if(CanProcessBytesRead(bytesRead))
+				{
+					state.HeaderOffset+=bytesRead;
+
+					if(state.HeaderOffset!=this.HeaderSize)
+					{
+						BeginReadHeader(state);
+					}
+					else
+					{
+						state.Header=CreateHeader(state.HeaderBuffer);
+						int bodySize=GetBodySize(state.Header);
+						state.BodyBuffer=new byte[bodySize];
+						BeginReadBody(state);
+					}
+				}
+			});
+		}
+
+		private void BeginReadBody(State state)
+		{
+			HandleNetworkCall(()=>
+			{
+				m_Stream.BeginRead(state.BodyBuffer,state.BodyOffset,state.BodyBuffer.Length-state.BodyOffset,ar=>EndReadBody(ar,state),null);
+			});
+		}
+
+		private void EndReadBody(IAsyncResult result, State state)
+		{
+			HandleNetworkCall(()=>
+			{
+				int bytesRead=m_Stream.EndRead(result);
+
+				if(CanProcessBytesRead(bytesRead))
+				{
+					state.BodyOffset+=bytesRead;
+
+					if(state.BodyOffset!=state.BodyBuffer.Length)
+					{
+						BeginReadBody(state);
+					}
+					else
+					{
+						state.Body=CreateBody(state.Header,state.BodyBuffer);
+						state.Handler(this,new SocketProcessorResult<THeader,TBody>(state.Header,state.Body));
+
+						Read(state.Handler);
+					}
+				}
+			});
+		}
+
+		private void HandleNetworkCall(Action action)
+		{
+			// TODO: Move to base
+
+			try
+			{
+				action();
+			}
+			catch
+			{
+				if(Interlocked.Read(ref m_Disposed)!=1)
+				{
+					OnNetworkFault(EventArgs.Empty);
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			if(m_Socket!=null)
+			{
+				Interlocked.Exchange(ref m_Disposed,1);
+
+				MethodCall.AllowFail(()=>m_Stream.Close());
+				MethodCall.AllowFail(()=>m_Socket.Close());
+
+				m_Socket=null;
+			}
+		}
+
+		class State
+		{
+			public Action<SocketProcessor,SocketProcessorResult<THeader,TBody>> Handler;
+
+			public byte[] HeaderBuffer;
+			public int HeaderOffset;
+			public THeader Header;
+
+			public byte[] BodyBuffer;
+			public int BodyOffset;
+			public TBody Body;
+		}
+	}
+}
---
+++ CSharp/Source/Arrow/Arrow/Net/SocketProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Arrow.Net
+{
+	public abstract class SocketProcessor
+	{
+		public event EventHandler<EventArgs> Disconnected;
+		public event EventHandler<EventArgs> NetworkFault;
+
+		private long m_KeepReading=1;
+
+		public bool KeepReading
+		{
+			get{return Interlocked.Read(ref m_KeepReading)==1;}
+			set
+			{
+				Interlocked.Exchange(ref m_KeepReading,(value ? 1 : 0));
+			}
+		}
+
+		protected bool CanProcessBytesRead(int bytesRead)
+		{
+			if(bytesRead==0)
+			{
+				OnDisconnected(EventArgs.Empty);
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+
+		protected void OnDisconnected(EventArgs args)
+		{
+			var handler=this.Disconnected;
+			if(handler!=null) handler(this,args);
+		}
+
+		protected void OnNetworkFault(EventArgs args)
+		{
+			var handler=this.NetworkFault;
+			if(handler!=null) handler(this,args);
+		}
+	}
+}
