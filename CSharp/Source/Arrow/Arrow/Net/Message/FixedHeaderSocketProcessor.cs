7145545f9219b45f79a0cce651bc5f29be513559
foxesknow <seanbatten58@gmail.com>
2015-03-15 11:44:02 +0000

    Refactor

--- CSharp/Source/Arrow/Arrow/Net/Message/FixedHeaderSocketProcessor.cs
+++ CSharp/Source/Arrow/Arrow/Net/Message/FixedHeaderSocketProcessor.cs
@@ -8,8 +8,6 @@ using System.Net.Sockets;
 using System.Threading;
 using System.IO;
 
-using Arrow.Execution;
-
 namespace Arrow.Net.Message
 {
 	/// <summary>
@@ -17,10 +15,8 @@ namespace Arrow.Net.Message
 	/// </summary>
 	/// <typeparam name="THeader">The type of the header</typeparam>
 	/// <typeparam name="TBody">The type of the body</typeparam>
-	public class FixedHeaderSocketProcessor<THeader,TBody> : SocketProcessor, IDisposable
+	public class FixedHeaderSocketProcessor<THeader,TBody> : SocketProcessor
 	{
-		private readonly Socket m_Socket;
-
 		private readonly IMessageFactory<THeader,TBody> m_MessageFactory;
 		private readonly IMessageProcessor<THeader,TBody> m_MessageProcessor;		
 
@@ -30,18 +26,15 @@ namespace Arrow.Net.Message
 		/// <param name="socket">The socket to process</param>
 		/// <param name="messageFactory">The message factory class that will create the header and bodies</param>
 		/// <param name="messageProcessor">The message processor that will handle the messages</param>
-		public FixedHeaderSocketProcessor(Socket socket, IMessageFactory<THeader,TBody> messageFactory, IMessageProcessor<THeader,TBody> messageProcessor) : base(messageProcessor)
+		public FixedHeaderSocketProcessor(Socket socket, IMessageFactory<THeader,TBody> messageFactory, IMessageProcessor<THeader,TBody> messageProcessor) : base(socket,messageProcessor)
 		{
-			if(socket==null) throw new ArgumentNullException("socket");
 			if(messageFactory==null) throw new ArgumentNullException("messageFactory");
 			if(messageProcessor==null) throw new ArgumentNullException("messageProcessor");
 
-			m_Socket=socket;
 			m_MessageFactory=messageFactory;
 			m_MessageProcessor=messageProcessor;
-
-			//m_Stream=new NetworkStream(socket);
 		}
+
 
 		/// <summary>
 		/// Starts the asynchronous read loop
@@ -52,114 +45,33 @@ namespace Arrow.Net.Message
 			State state=new State(headerSize);
 
 			BeginReadHeader(state);
-		}
-
-		/// <summary>
-		/// Writes data to the socket
-		/// </summary>
-		/// <param name="buffer">The data to write</param>
-		/// <param name="offset">The start of the data within the buffer</param>
-		/// <param name="size">How much data to write</param>
-		public override void Write(byte[] buffer, int offset, int size)
-		{
-			if(buffer==null) throw new ArgumentNullException("buffer");
-
-			bool success=HandleNetworkCall(()=>
-			{
-				m_Socket.Send(buffer,offset,size,SocketFlags.None);
-			});
-
-			if(success==false) throw new IOException("write failed");
-		}
-
-		/// <summary>
-		/// Writes data to the socket
-		/// </summary>
-		/// <param name="buffer">The data to write</param>
-		/// <param name="offset">The start of the data within the buffer</param>
-		/// <param name="size">How much data to write</param>
-		/// <returns>A task that will be signalled when the write completes</returns>
-		public override Task<SocketProcessor> WriteAsync(byte[] buffer, int offset, int size)
-		{
-			if(buffer==null) throw new ArgumentNullException("buffer");
-
-			var completionSource=new TaskCompletionSource<SocketProcessor>();
-
-			bool success=HandleNetworkCall(()=>
-			{
-				m_Socket.BeginSend(buffer,offset,size,SocketFlags.None,EndWrite,completionSource);
-			});
-
-			if(success==false)
-			{
-				completionSource.SetException(new IOException("write failed"));
-			}
-
-			return completionSource.Task;
-		}
-
-		private void EndWrite(IAsyncResult result)
-		{
-			var completionSource=(TaskCompletionSource<SocketProcessor>)result.AsyncState;
-
-			bool success=HandleNetworkCall(()=>
-			{
-				m_Socket.EndSend(result);
-				completionSource.SetResult(this);
-				success=true;
-			});
-
-			if(success==false)
-			{
-				completionSource.SetException(new IOException("write failed"));
-			}
-		}
-
-		/// <summary>
-		/// Closes the socket processor
-		/// </summary>
-		public override void Close()
-		{
-			if(IsClosed()==false)
-			{
-				/*
-				 * We're going to start closing the stream and the socket.
-				 * However, they may have pending read/writes on them, so we need to 
-				 * make sure we know we're closing to handle any exceptions we get
-				 * from their Read/Write methods
-				 */
-				FlagAsClosed();
-
-				MethodCall.AllowFail(()=>m_Socket.Dispose());
-			}
-		}
+		}		
 
 		private void BeginReadHeader(State state)
 		{
-			HandleNetworkCall(()=>
+			HandleNetworkCall(state,(localState)=>
 			{
-				m_Socket.BeginReceive
+				this.Socket.BeginReceive
 				(
 					state.HeaderBuffer,
 					state.HeaderOffset,
 					state.HeaderBuffer.Length-state.HeaderOffset,
 					SocketFlags.None,
 					EndReadHeader,
-					state
+					localState
 				);
 			});
 		}
 
 		private void EndReadHeader(IAsyncResult result)
 		{
-			State state=(State)result.AsyncState;
-
-			HandleNetworkCall(()=>
+			HandleNetworkCall(result,(ar)=>
 			{
-				int bytesRead=m_Socket.EndReceive(result);
+				int bytesRead=this.Socket.EndReceive(ar);
 
 				if(CanProcessBytesRead(bytesRead))
 				{
+					State state=(State)ar.AsyncState;
 					state.HeaderOffset+=bytesRead;
 
 					if(state.HeaderOffset!=m_MessageFactory.HeaderSize)
@@ -180,30 +92,29 @@ namespace Arrow.Net.Message
 
 		private void BeginReadBody(State state)
 		{
-			HandleNetworkCall(()=>
+			HandleNetworkCall(state,(localState)=>
 			{
-				m_Socket.BeginReceive
+				this.Socket.BeginReceive
 				(
 					state.BodyBuffer,
 					state.BodyOffset,
 					state.BodyBuffer.Length-state.BodyOffset,
 					SocketFlags.None,
 					EndReadBody,
-					state
+					localState
 				);
 			});
 		}
 
 		private void EndReadBody(IAsyncResult result)
 		{
-			State state=(State)result.AsyncState;
-
-			HandleNetworkCall(()=>
+			HandleNetworkCall(result,(ar)=>
 			{
-				int bytesRead=m_Socket.EndReceive(result);
+				int bytesRead=this.Socket.EndReceive(ar);
 
 				if(CanProcessBytesRead(bytesRead))
 				{
+					State state=(State)ar.AsyncState;
 					state.BodyOffset+=bytesRead;
 
 					if(state.BodyOffset!=state.BodyBuffer.Length)
@@ -236,15 +147,7 @@ namespace Arrow.Net.Message
 		protected override void OnDisconnected()
 		{
 			m_MessageProcessor.HandleDisconnect(this);
-		}		
-
-		/// <summary>
-		/// Disposes of the processor
-		/// </summary>
-		public void Dispose()
-		{
-			Close();
-		}
+		}				
 
 		class State
 		{
--- CSharp/Source/Arrow/Arrow/Net/Message/SocketProcessor.cs
+++ CSharp/Source/Arrow/Arrow/Net/Message/SocketProcessor.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using Arrow.Execution;
 
 namespace Arrow.Net.Message
 {
 	/// <summary>
 	/// Base class for all socket processors
 	/// </summary>
-	public abstract class SocketProcessor
+	public abstract class SocketProcessor : IDisposable
 	{
+		private readonly Socket m_Socket;
 		private readonly IMessageProcessor m_MessageProcessor;
 
 		private long m_Closed;
@@ -19,12 +23,23 @@ namespace Arrow.Net.Message
 		/// <summary>
 		/// Initializes the instance
 		/// </summary>
+		/// <param name="socket">The socket to communicate over</param>
 		/// <param name="messageProcessor">The message processor to use</param>
-		protected SocketProcessor(IMessageProcessor messageProcessor)
+		protected SocketProcessor(Socket socket, IMessageProcessor messageProcessor)
 		{
+			if(socket==null) throw new ArgumentNullException("socket");
 			if(messageProcessor==null) throw new ArgumentNullException("messageProcessor");
 
+			m_Socket=socket;
 			m_MessageProcessor=messageProcessor;
+		}
+
+		/// <summary>
+		/// The socket being used
+		/// </summary>
+		protected Socket Socket
+		{
+			get{return m_Socket;}
 		}
 
 		/// <summary>
@@ -88,19 +103,57 @@ namespace Arrow.Net.Message
 		}
 
 		/// <summary>
+		/// Executes a piece of network code and deals with any errors
+		/// </summary>
+		/// <param name="state">State to pass into the action</param>
+		/// <param name="action">The network code to execute</param>
+		/// <returns>true if the network code executes successfully, otherwise false</returns>
+		protected bool HandleNetworkCall<T>(T state, Action<T> action)
+		{
+			try
+			{
+				action(state);
+				return true;
+			}
+			catch
+			{
+				if(Interlocked.Read(ref m_Closed)!=1)
+				{
+					m_MessageProcessor.HandleNetworkFault(this);
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
 		/// Starts reading messages from the socket
 		/// </summary>
 		public abstract void Start();
 
 		/// <summary>
-		/// Closes the socket processor
-		/// </summary>
-		public abstract void Close();
-
-		/// <summary>
 		/// Called when a network disconnect is detected
 		/// </summary>
 		protected abstract void OnDisconnected();
+		
+
+		/// <summary>
+		/// Writes data to the socket
+		/// </summary>
+		/// <param name="buffer">The data to write</param>
+		/// <param name="offset">The start of the data within the buffer</param>
+		/// <param name="size">How much data to write</param>
+		public  void Write(byte[] buffer, int offset, int size)
+		{
+			if(buffer==null) throw new ArgumentNullException("buffer");
+
+			bool success=HandleNetworkCall(()=>
+			{
+				this.Socket.Send(buffer,offset,size,SocketFlags.None);
+			});
+
+			if(success==false) throw new IOException("write failed");
+		}
 
 
 		/// <summary>
@@ -110,15 +163,68 @@ namespace Arrow.Net.Message
 		/// <param name="offset">The start of the data within the buffer</param>
 		/// <param name="size">How much data to write</param>
 		/// <returns>A task that will be signalled when the write completes</returns>
-		public abstract Task<SocketProcessor> WriteAsync(byte[] buffer, int offset, int size);
+		public Task<SocketProcessor> WriteAsync(byte[] buffer, int offset, int size)
+		{
+			if(buffer==null) throw new ArgumentNullException("buffer");
+
+			var completionSource=new TaskCompletionSource<SocketProcessor>();
+
+			bool success=HandleNetworkCall(()=>
+			{
+				this.Socket.BeginSend(buffer,offset,size,SocketFlags.None,EndWrite,completionSource);
+			});
+
+			if(success==false)
+			{
+				completionSource.SetException(new IOException("write failed"));
+			}
+
+			return completionSource.Task;
+		}
+
+		private void EndWrite(IAsyncResult result)
+		{
+			bool success=HandleNetworkCall(result,(ar)=>
+			{
+				var completionSource=(TaskCompletionSource<SocketProcessor>)ar.AsyncState;
+
+				this.Socket.EndSend(ar);
+				completionSource.SetResult(this);
+				success=true;
+			});
+
+			if(success==false)
+			{
+				var completionSource=(TaskCompletionSource<SocketProcessor>)result.AsyncState;
+				completionSource.SetException(new IOException("write failed"));
+			}
+		}	
 
 		/// <summary>
-		/// Writes data to the socket
+		/// Closes the socket processor
 		/// </summary>
-		/// <param name="buffer">The data to write</param>
-		/// <param name="offset">The start of the data within the buffer</param>
-		/// <param name="size">How much data to write</param>
-		public abstract void Write(byte[] buffer, int offset, int size);
+		public virtual void Close()
+		{
+			if(IsClosed()==false)
+			{
+				/*
+				 * We're going to start closing the stream and the socket.
+				 * However, they may have pending read/writes on them, so we need to 
+				 * make sure we know we're closing to handle any exceptions we get
+				 * from their Read/Write methods
+				 */
+				FlagAsClosed();
 
+				MethodCall.AllowFail(()=>m_Socket.Dispose());
+			}
+		}
+
+		/// <summary>
+		/// Disposes of the processor
+		/// </summary>
+		public void Dispose()
+		{
+			Close();
+		}
 	}
 }
