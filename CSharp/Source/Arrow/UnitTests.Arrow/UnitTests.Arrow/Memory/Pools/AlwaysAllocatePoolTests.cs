c1adac6dd506f5136f1646d4fae97d7a918f6324
foxesknow <seanbatten58@gmail.com>
2015-03-28 11:27:12 +0000

    Pool unit tests

---
+++ CSharp/Source/Arrow/UnitTests.Arrow/UnitTests.Arrow/Memory/Pools/AlwaysAllocatePoolTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Arrow.Memory.Pools;
+
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace UnitTests.Arrow.Memory.Pools
+{
+	[TestFixture]
+	public class AlwaysAllocatePoolTests
+	{
+		[Test]
+		public void Checkout()
+		{
+			var pool=new AlwaysAllocatePool();
+
+			for(int i=0; i<10; i++)
+			{
+				int size=(i+1)*819;
+				var buffer=pool.Checkout(size);
+
+				Assert.That(buffer,Is.Not.Null);
+				Assert.That(buffer.Length,Is.EqualTo(size));
+
+				pool.Checkin(buffer);
+			}
+		}
+	}
+}
