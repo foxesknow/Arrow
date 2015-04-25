aeb2e96aa457c36d27212417866e8157f3e3e7cd
foxesknow <seanbatten58@gmail.com>
2015-04-25 18:19:18 +0100

    Added ByteSegmentStream

---
+++ CSharp/Source/Arrow/Arrow/IO/ByteSegmentStream.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Arrow.IO
+{
+	/// <summary>
+	/// Allows part of an array to be used as a stream
+	/// </summary>
+	public class ByteSegmentStream : Stream
+	{
+		private readonly ArraySegment<byte> m_Segment;
+		private long m_Position=0;
+
+		/// <summary>
+		/// Initializes the instance
+		/// </summary>
+		/// <param name="data">The data to stream</param>
+		public ByteSegmentStream(byte[] data) : this(new ArraySegment<byte>(data))
+		{
+		}
+
+		/// <summary>
+		/// Initializes the instance
+		/// </summary>
+		/// <param name="data">The data to stream</param>
+		/// <param name="offset">The offset in the data to start from</param>
+		/// <param name="count">The number of bytes in the array to use</param>
+		public ByteSegmentStream(byte[] data, int offset, int count) : this(new ArraySegment<byte>(data,offset,count))
+		{
+		}
+
+		/// <summary>
+		/// Initializes the instance
+		/// </summary>
+		/// <param name="segment">The array segment to use</param>
+		public ByteSegmentStream(ArraySegment<byte> segment)
+		{
+			m_Segment=segment;
+		}
+
+		/// <summary>
+		/// How much space is left in the stream based on its current position
+		/// </summary>
+		public int AvailableSpace
+		{
+			get{return (int)(m_Segment.Count-m_Position);}
+		}
+
+		/// <summary>
+		/// Returns true
+		/// </summary>
+		public override bool CanRead
+		{
+			get{return true;}
+		}
+
+		/// <summary>
+		/// Returns true
+		/// </summary>
+		public override bool CanSeek
+		{
+			get{return true;}
+		}
+
+		/// <summary>
+		/// Returns true
+		/// </summary>
+		public override bool CanWrite
+		{
+			get{return true;}
+		}
+
+		/// <summary>
+		/// Does nothing
+		/// </summary>
+		public override void Flush()
+		{
+		}
+
+		/// <summary>
+		/// Returns the total number of bytes
+		/// </summary>
+		public override long Length
+		{
+			get{return m_Segment.Count;}
+		}
+
+		/// <summary>
+		/// The current position
+		/// </summary>
+		public override long Position
+		{
+			get{return m_Position;}
+			set
+			{
+				if(value<0 || value>=m_Segment.Count) throw new IOException("invalid position");
+				m_Position=value;
+			}
+		}
+
+		/// <summary>
+		/// Reads data from the underlying array
+		/// </summary>
+		/// <param name="buffer">The buffer to populate</param>
+		/// <param name="offset">Where to start in the buffer</param>
+		/// <param name="count">The number of bytes to read</param>
+		/// <returns>The number of bytes read</returns>
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			if(buffer==null) throw new ArgumentNullException("buffer");
+			if(offset<0) throw new ArgumentOutOfRangeException("offset");
+			if(count<0) throw new ArgumentOutOfRangeException("count");
+			if(offset+count>buffer.Length) throw new ArgumentException("read beyond buffer");
+
+			int available=m_Segment.Count-(int)m_Position;
+			int bytesToRead=Math.Min(count,available);
+
+			var underlying=m_Segment.Array;
+			int underlyingOffset=m_Segment.Offset;
+
+			for(int i=0; i<bytesToRead; i++)
+			{
+				buffer[offset+i]=underlying[underlyingOffset+m_Position];
+				m_Position++;
+			}
+
+			return bytesToRead;
+		}
+
+		/// <summary>
+		/// Seeks to a new position in the array
+		/// </summary>
+		/// <param name="offset">The offset to seek to</param>
+		/// <param name="origin">The origin to apply the offset to</param>
+		/// <returns>The new position in the stream</returns>
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			long newPosition=0;
+
+			switch(origin)
+			{
+				case SeekOrigin.Begin:
+					newPosition=offset;
+					break;
+
+				case SeekOrigin.Current:
+					newPosition=m_Position+offset;
+					break;
+
+				case SeekOrigin.End:
+					newPosition=m_Segment.Count+offset;
+					break;
+
+				default:
+					throw new IOException("unsupported origin: "+origin.ToString());
+			}
+
+			if(newPosition<0 || newPosition>=m_Segment.Count) throw new IOException("cannot seek beyond segment");
+
+			m_Position=newPosition;
+			return m_Position;
+		}
+
+		/// <summary>
+		/// Not implemented
+		/// </summary>
+		/// <param name="value"></param>
+		public override void SetLength(long value)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Writes data to the underlying array
+		/// </summary>
+		/// <param name="buffer">The buffer to take the data from</param>
+		/// <param name="offset">Where to start in the buffer</param>
+		/// <param name="count">The number of bytes to write</param>
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			if(buffer==null) throw new ArgumentNullException("buffer");
+			if(offset<0) throw new ArgumentOutOfRangeException("offset");
+			if(count<0) throw new ArgumentOutOfRangeException("count");
+			if(offset+count>buffer.Length) throw new ArgumentException("write beyond buffer");
+
+			if(m_Position+count>m_Segment.Count) throw new IOException("not enough space");
+
+			var underlying=m_Segment.Array;
+			int underlyingOffset=m_Segment.Offset;
+
+			for(int i=0; i<count; i++)
+			{
+				underlying[underlyingOffset+m_Position]=buffer[offset+i];
+				m_Position++;
+			}
+		}
+	}
+}
---
+++ CSharp/Source/Arrow/UnitTests.Arrow/UnitTests.Arrow/IO/ByteSegmentStreamTests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Arrow.IO;
+
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace UnitTests.Arrow.IO
+{
+	[TestFixture]
+	public class ByteSegmentStreamTests
+	{
+		[Test]
+		public void Construction()
+		{
+			byte[] data=new byte[100];
+			
+			var stream1=new ByteSegmentStream(data);
+			Assert.That(stream1.Position,Is.EqualTo(0));
+			Assert.That(stream1.AvailableSpace,Is.EqualTo(data.Length));
+			Assert.That(stream1.Length,Is.EqualTo(data.Length));
+
+			var stream2=new ByteSegmentStream(data,10,90);
+			Assert.That(stream2.Position,Is.EqualTo(0));
+			Assert.That(stream2.AvailableSpace,Is.EqualTo(90));
+			Assert.That(stream2.Length,Is.EqualTo(90));
+
+			var stream3=new ByteSegmentStream(new ArraySegment<byte>(data,2,3));
+			Assert.That(stream3.Position,Is.EqualTo(0));
+			Assert.That(stream3.AvailableSpace,Is.EqualTo(3));
+			Assert.That(stream3.Length,Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Write_FromBegin()
+		{
+			var data=new byte[20];
+			var segment=new ArraySegment<byte>(data);
+
+			using(var stream=new ByteSegmentStream(segment))
+			{
+				Assert.That(stream.Position,Is.EqualTo(0));
+				Assert.That(stream.AvailableSpace,Is.EqualTo(data.Length));
+
+				var toWrite=new Byte[]{1,2,3};
+				stream.Write(toWrite,0,toWrite.Length);
+
+				Assert.That(stream.Position,Is.EqualTo(toWrite.Length));
+				Assert.That(stream.AvailableSpace,Is.EqualTo(data.Length-toWrite.Length));
+			}
+		}
+
+		[Test]
+		public void Write_FromMid()
+		{
+			var data=new byte[20];
+			var segment=new ArraySegment<byte>(data);
+
+			using(var stream=new ByteSegmentStream(segment))
+			{
+				Assert.That(stream.Position,Is.EqualTo(0));
+				Assert.That(stream.AvailableSpace,Is.EqualTo(data.Length));
+
+				stream.Position=10;
+
+				var toWrite=new Byte[]{1,2,3};
+				stream.Write(toWrite,0,toWrite.Length);
+
+				stream.Position=0;
+				byte[] readData=new byte[10];
+
+				stream.Read(readData,0,readData.Length);
+
+				for(int i=0; i<readData.Length; i++)
+				{
+					CheckAllValues(readData,0);
+				}
+			}
+		}
+
+		[Test]
+		public void Write_CheckOverflow()
+		{
+			byte[] data=new byte[200];
+			for(int i=0; i<data.Length; i++) data[i]=99;
+
+			using(var stream=new ByteSegmentStream(data,50,100))
+			{
+				var zeros=new byte[100];
+				stream.Write(zeros,0,zeros.Length);
+
+				for(int i=0; i<data.Length; i++)
+				{
+					if(i>=0 && i<=49) Assert.That(data[i],Is.EqualTo(99));
+					if(i>=50 && i<=149) Assert.That(data[i],Is.EqualTo(0));
+					if(i>=150 && i<=199) Assert.That(data[i],Is.EqualTo(99));
+				}
+			}
+		}
+
+		[Test]
+		public void Read_AllAvailable()
+		{
+			byte[] data=new byte[200];
+			for(int i=0; i<data.Length; i++) data[i]=99;
+
+			using(var stream=new ByteSegmentStream(data))
+			{
+				byte[] readData=new byte[50];
+
+				int bytesRead=stream.Read(readData,0,readData.Length);
+				Assert.That(bytesRead,Is.EqualTo(readData.Length));
+				CheckAllValues(readData,99);
+
+				Assert.That(stream.Position,Is.EqualTo(readData.Length));
+			}
+		}
+
+		[Test]
+		public void Read_SomeAvailable()
+		{
+			byte[] data=new byte[200];
+			for(int i=0; i<data.Length; i++) data[i]=99;
+
+			using(var stream=new ByteSegmentStream(data))
+			{
+				byte[] readData=new byte[250];
+
+				int bytesRead=stream.Read(readData,0,readData.Length);
+				Assert.That(bytesRead,Is.EqualTo(data.Length));
+				CheckAllValues(readData,0,bytesRead,99);
+
+				Assert.That(stream.Position,Is.EqualTo(data.Length));
+			}
+		}
+
+		[TestCase(0,10,SeekOrigin.Begin,10)]
+		[TestCase(5,3,SeekOrigin.Begin,3)]
+		[TestCase(5,3,SeekOrigin.Current,8)]
+		[TestCase(20,-3,SeekOrigin.Current,17)]
+		[TestCase(100,-12,SeekOrigin.End,88)]
+		public void Seek(long basePosition, long offset, SeekOrigin origin, long expectedPosition)
+		{
+			byte[] data=new byte[100];
+			using(var stream=new ByteSegmentStream(data))
+			{
+				stream.Position=basePosition;
+				var newPosition=stream.Seek(offset,origin);
+
+				Assert.That(newPosition,Is.EqualTo(expectedPosition));
+				Assert.That(stream.Position,Is.EqualTo(expectedPosition));
+			}
+		}
+
+		private void CheckAllValues(byte[] data, int expectedByte)
+		{
+			for(int i=0; i<data.Length; i++)
+			{
+				Assert.That(data[i],Is.EqualTo(expectedByte));
+			}
+		}
+
+		private void CheckAllValues(byte[] data, int offset, int count, int expectedByte)
+		{
+			for(int i=0; i<count; i++)
+			{
+				Assert.That(data[offset+i],Is.EqualTo(expectedByte));
+			}
+		}
+	}
+}
