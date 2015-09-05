6bf39c1fe86b3ad7e62d2e6cc41ae5948569d89e
foxesknow <seanbatten58@gmail.com>
2015-09-05 13:03:27 +0100

    Added cmdlet to manipulate the host title

---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.Tools/GetTitleCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.Tools
+{
+	[Cmdlet(VerbsCommon.Get,"Title")]
+	[OutputType(typeof(string))]
+	public class GetTitleCommand : PSCmdlet
+	{
+		protected override void ProcessRecord()
+		{
+			WriteObject(this.Host.UI.RawUI.WindowTitle);
+		}
+	}
+}
---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.Tools/SetTitleCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.Tools
+{
+	[Cmdlet(VerbsCommon.Set,"Title")]
+	[OutputType(typeof(string))]
+	public class SetTitleCommand : PSCmdlet
+	{
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The title for the PowerShell window",
+			ValueFromPipeline=true,
+			ValueFromPipelineByPropertyName=true,
+			Position=0
+		)]
+		[ValidateNotNullOrWhitespace]
+		public string Title{get;set;}
+
+		protected override void ProcessRecord()
+		{
+			this.Host.UI.RawUI.WindowTitle=this.Title;
+			WriteObject(this.Title);
+		}
+	}
+}
