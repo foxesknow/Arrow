d7cced3dc3dfe774b8ec6ece3b5be5556e05f66a
foxesknow <seanbatten58@gmail.com>
2015-09-05 14:38:50 +0100

    Added Read-Binary cmdlet

---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.Tools/BinaryRow.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.Tools
+{
+	/// <summary>
+	/// Represents a row of binary data
+	/// </summary>
+	public class BinaryRow
+	{
+		private readonly long m_Address;
+		private readonly byte[] m_Data;
+		private readonly int m_BlockSize;
+
+		private string m_AsString;
+
+		public BinaryRow(long address, byte[] data, int blockSize)
+		{
+			m_Address=address;
+			m_Data=data;
+			m_BlockSize=blockSize;
+		}
+
+		/// <summary>
+		/// The address the data came from
+		/// </summary>
+		public long Address
+		{
+			get{return m_Address;}
+		}
+
+		/// <summary>
+		/// The binary data
+		/// </summary>
+		public byte[] Data
+		{
+			get{return m_Data;}
+		}		
+
+		private string Render()
+		{
+			var builder=new StringBuilder();
+
+			if(m_Address>=int.MaxValue)
+			{
+				builder.AppendFormat("{0:x16} ",m_Address);
+			}
+			else
+			{
+				builder.AppendFormat("{0:x8} ",m_Address);
+			}
+
+			// Write the data in hex
+			for(int i=0; i<m_BlockSize; i++)
+			{
+				if(i<m_Data.Length)
+				{
+					builder.AppendFormat("{0:x2} ",m_Data[i]);
+				}
+				else
+				{
+					builder.AppendFormat("   ");
+				}
+			}
+
+			builder.Append(" ");
+
+			// And how in printable format
+			for(int i=0; i<m_Data.Length; i++)
+			{
+				byte b=m_Data[i];
+
+				if(b>=32 && b<=127)
+				{
+					builder.Append((char)b);
+				}
+				else
+				{
+					builder.Append('.');
+				}
+			}
+
+			return builder.ToString();
+		}
+
+		public override string ToString()
+		{
+			if(m_AsString==null)
+			{
+				m_AsString=Render();
+			}
+
+			return m_AsString;
+		}
+	}
+}
---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.Tools/ReadBinaryCommand.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.Tools
+{
+	/// <summary>
+	/// Reads binary data from a file and outputs it as a BinaryRow object
+	/// 
+	/// To get a traditional hex dump style output do:
+	///   Read-Binary -Filename file.dat | Write-Host
+	/// </summary>
+	[Cmdlet("Read","Binary")]
+	[OutputType(typeof(BinaryRow))]
+	public class ReadBinaryCommand : PSCmdlet
+	{
+		public ReadBinaryCommand()
+		{
+			this.BlockSize=16;
+		}
+
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The file whose binary data should be read",
+			ValueFromPipeline=true,
+			ValueFromPipelineByPropertyName=true
+		)]
+		[ValidateNotNullOrWhitespace]
+		public string Filename{get;set;}
+
+		[Parameter
+		(
+			HelpMessage="The file whose binary data should be written"
+		)]
+		public int BlockSize{get;set;}
+
+		protected override void ProcessRecord()		
+		{
+			if(this.BlockSize<1) throw new ArgumentException("BlockSize must be at least 1");
+
+			using(var stream=OpenFile())
+			{
+				long address=0;
+
+				byte[] buffer=new byte[this.BlockSize];
+
+				int bytesRead=0;
+				while((bytesRead=stream.Read(buffer,0,buffer.Length))!=0)
+				{
+					byte[] data=new byte[bytesRead];
+					Array.Copy(buffer,data,bytesRead);
+
+					var row=new BinaryRow(address,data,buffer.Length);					
+					WriteObject(row);
+
+					address+=bytesRead;
+				}
+			}
+		}
+
+		private Stream OpenFile()
+		{
+			var filename=this.GetUnresolvedProviderPathFromPSPath(this.Filename);
+			return File.OpenRead(filename);
+		}
+	}
+}
