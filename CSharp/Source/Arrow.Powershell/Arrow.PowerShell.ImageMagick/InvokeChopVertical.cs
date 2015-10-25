71ebc052ebfa76bcf345825c31f09efa2575e554
foxesknow <seanbatten58@gmail.com>
2015-10-25 16:16:30 +0000

    Addec Chroma command

---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeChopVertical.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.ImageMagick
+{
+	[Cmdlet("Invoke","ChopVertical")]
+	public class InvokeChopVertical : ImageCmdlet
+	{
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The Y offset from origin"
+		)]
+		public int Offset{get;set;}
+
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The height of the part to chop vertically"
+		)]
+		public int Height{get;set;}
+
+		protected override void Apply()
+		{
+			using(var image=GetMagickImage())
+			{
+				image.ChopVertical(this.Offset,this.Height);
+				WriteImage(image);
+			}
+		}
+	}
+}
---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeChroma.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.ImageMagick
+{
+	[Cmdlet("Invoke","Chop")]
+	public class InvokeChroma : ImageCmdlet
+	{
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The chromacity to change"
+		)]
+		[ValidateSet("Blue","Green","Red","WhitePoint")]
+		public string Type{get;set;}
+
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The X coordinate"
+		)]
+		public double X{get;set;}
+
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The Y coordinate"
+		)]
+		public double Y{get;set;}
+
+		protected override void Apply()
+		{
+			using(var image=GetMagickImage())
+			{
+				switch(this.Type)
+				{
+					case "Red":
+						image.ChromaRedPrimary(this.X,this.Y);
+						break;
+
+					case "Greed":
+						image.ChromaGreenPrimary(this.X,this.Y);
+						break;
+
+					case "Blue":
+						image.ChromaBluePrimary(this.X,this.Y);
+						break;
+
+					case "WhitePoint":
+						image.ChromaRedPrimary(this.X,this.Y);
+						break;
+
+					default:
+						throw new InvalidOperationException("Unknown type: "+this.Type);
+				}
+
+				WriteImage(image);
+			}
+		}
+	}
+}
