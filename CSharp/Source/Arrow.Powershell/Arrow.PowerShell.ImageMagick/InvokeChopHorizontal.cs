52738b4dc8181108a9c404ed152ed60c541f141a
foxesknow <seanbatten58@gmail.com>
2015-10-25 15:50:45 +0000

    Added ChopHorizontal

---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeChopHorizontal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.ImageMagick
+{
+	[Cmdlet("Invoke","ChopHorizontal")]
+	public class InvokeChopHorizontal : ImageCmdlet
+	{
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The X offset from origin"
+		)]
+		public int Offset{get;set;}
+
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="The width of the part to chop horizontally"
+		)]
+		public int Width{get;set;}
+
+		protected override void Apply()
+		{
+			using(var image=GetMagickImage())
+			{
+				image.ChopHorizontal(this.Offset,this.Width);
+				WriteImage(image);
+			}
+		}
+	}
+}
