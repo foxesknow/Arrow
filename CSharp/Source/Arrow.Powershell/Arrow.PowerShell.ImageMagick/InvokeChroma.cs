f21829c069611a97797aa783c6e2022b45f4d4a1
foxesknow <seanbatten58@gmail.com>
2015-10-25 16:21:32 +0000

    Fixed chroma name

--- CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeChroma.cs
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeChroma.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Arrow.PowerShell.ImageMagick
 {
-	[Cmdlet("Invoke","Chop")]
+	[Cmdlet("Invoke","Chroma")]
 	public class InvokeChroma : ImageCmdlet
 	{
 		[Parameter
