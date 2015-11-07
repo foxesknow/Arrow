a886393a6fb1e1c9f57da639ad67bcf64fc45cb6
foxesknow <seanbatten58@gmail.com>
2015-11-07 12:40:08 +0000

    New commands

---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeContrast.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.PowerShell.ImageMagick
+{
+	[Cmdlet("Invoke","Contrast")]
+	public class InvokeContrast : ImageCmdlet
+	{
+		[Parameter
+		(
+			Mandatory=true,
+			HelpMessage="True to enhance the contrast and false to reduce the contrast"
+		)]
+		public bool? Enhance{get;set;}
+
+		protected override void Apply()
+		{
+			using(var image=GetMagickImage())
+			{
+				if(this.Enhance.HasValue)
+				{
+					image.Contrast(this.Enhance.Value);
+				}
+				else
+				{
+					image.Contrast();
+				}
+
+				WriteImage(image);
+			}
+		}
+	}
+}
---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeContrastStretch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
+
+namespace Arrow.PowerShell.ImageMagick
+{
+	[Cmdlet("Invoke","ContrastStretch",DefaultParameterSetName="BPoint")]
+	public class InvokeContrastStretch : ImageCmdlet
+	{
+		[Parameter(Mandatory=true,ParameterSetName="BPoint",HelpMessage="The black point")]
+		[Parameter(Mandatory=true,ParameterSetName="BWPoint",HelpMessage="The black point")]
+		[Parameter(Mandatory=true,ParameterSetName="BWCPoint",HelpMessage="The black point")]
+		public double BlackPoint{get;set;}
+
+		[Parameter(Mandatory=true,ParameterSetName="BWPoint",HelpMessage="The black point")]
+		[Parameter(Mandatory=true,ParameterSetName="BWCPoint",HelpMessage="The white point")]
+		public double WhitekPoint{get;set;}
+
+		[Parameter(Mandatory=true,ParameterSetName="BWCPoint",HelpMessage="The channels to contrast stretch")]
+		public Channels Channels{get;set;}
+
+		protected override void Apply()
+		{
+			using(var image=GetMagickImage())
+			{
+				switch(this.ParameterSetName)
+				{
+					case "BPoint":
+						image.ContrastStretch((Percentage)this.BlackPoint);
+						break;
+
+					case "BWPoint":
+						image.ContrastStretch((Percentage)this.BlackPoint,(Percentage)this.WhitekPoint);
+						break;
+
+					case "BWCPoint":
+						image.ContrastStretch((Percentage)this.BlackPoint,(Percentage)this.WhitekPoint,this.Channels);
+						break;
+
+					default:
+						throw new InvalidOperationException("Unknown parameter set: "+this.ParameterSetName);
+				}
+			}
+		}
+	}
+}
