fd8486455efbf359daceb0720043d11c7823fefd
foxesknow <seanbatten58@gmail.com>
2015-11-14 19:15:34 +0000

    Added InvokeColorMap

---
+++ CSharp/Source/Arrow.Powershell/Arrow.PowerShell.ImageMagick/InvokeColorMap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
+
+namespace Arrow.PowerShell.ImageMagick
+{
+	[Cmdlet("Invoke","ColorMap")]
+	public class InvokeColorMap : ImageCmdlet
+	{
+		[Parameter(Mandatory=true,HelpMessage="The position index")]
+		public int Index{get;set;}
+
+		[Parameter(Mandatory=true,HelpMessage="The color")]
+		public MagickColor Color{get;set;}
+
+		protected override void Apply()
+		{
+			using(var image=GetMagickImage())
+			{
+				image.ColorMap(this.Index,this.Color);
+				WriteImage(image);
+			}
+		}
+	}
+}
