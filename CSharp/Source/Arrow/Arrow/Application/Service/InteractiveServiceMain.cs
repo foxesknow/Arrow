db3788f6ed5be3f98a176124498515360a6e2837
Sean <seanbatten58@gmail.com>
2016-07-10 16:56:41 +0100

    Refactor

---
+++ CSharp/Source/Arrow/Arrow/Application/Service/InteractiveServiceMain.cs
@@ -0,0 +1,48 @@
+﻿using Arrow.Execution;
+using Arrow.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arrow.Application.Service
+{
+	/// <summary>
+	/// A service which doesn't spawn a thread to execute the Start/Stop methods.
+	///
+	/// Because of this it is the responsibility of Start() to not block,
+	/// but return once it has started up the service infrastructure. This typically means
+	/// that the Start() method will have spawned its own threads.
+	/// 
+	/// </summary>
+	public abstract class InteractiveServiceMain : ServiceMain
+	{
+		private readonly ILog Log=LogManager.GetDefaultLog();
+		private ManualResetEvent m_StopEvent;
+
+		internal override void OnStart(string[] args)
+		{
+			m_StopEvent=new ManualResetEvent(false);
+			
+			Start(m_StopEvent,args);
+		}
+
+		internal override void OnStop()
+		{
+			try
+			{
+				m_StopEvent.Set();
+				Stop();
+			}
+			catch(Exception e)
+			{
+				Log.Error("Error stopping interactive service",e);
+			}
+
+			m_StopEvent.Dispose();
+			m_StopEvent=null;
+		}
+	}
+}
---
+++ CSharp/Source/Arrow/Arrow/Application/Service/ServiceMainHost.cs
@@ -0,0 +1,47 @@
+﻿using Arrow.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.Application.Service
+{
+	/// <summary>
+	/// Hosts a ServiceMain instance
+	/// </summary>
+	/// <typeparam name="TServiceMain"></typeparam>
+	public sealed class ServiceMainHost<TServiceMain> : InteractiveServiceBase where TServiceMain:ServiceMain,new()
+	{
+		private TServiceMain m_ServiceMain;
+
+		/// <summary>
+		/// Starts the service
+		/// </summary>
+		/// <param name="args"></param>
+		protected override void OnStart(string[] args)
+		{
+			m_ServiceMain=new TServiceMain();
+			m_ServiceMain.OnStart(args);
+		}
+
+		/// <summary>
+		/// Stops the service
+		/// </summary>
+		protected override void OnStop()
+		{
+			if(m_ServiceMain!=null)
+			{
+				m_ServiceMain.OnStop();
+
+				IDisposable disposer=m_ServiceMain as IDisposable;
+				if(disposer!=null)
+				{
+					MethodCall.AllowFail(disposer,d=>d.Dispose());
+				}
+
+				m_ServiceMain=null;
+			}
+		}
+	}
+}
