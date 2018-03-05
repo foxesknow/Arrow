5131693d26a0651d0e5164965f6cc1199681b127
Sean Batten <seanbatten58@gmail.com>
2018-03-05 21:42:56 +0000

    Added logging wrappers

--- CSharp/Source/Arrow/Apps/FSharpConsoleApplication/Program.fs
+++ CSharp/Source/Arrow/Apps/FSharpConsoleApplication/Program.fs
@@ -1,17 +1,19 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open Arrow.FSharp.Collections
+open Arrow.FSharp.Logging
 
-let fib r n =
-  match n with
-  | 0 -> 0
-  | 1 -> 1
-  | n -> r (n - 1) + r (n - 2)
+open FSharp.Core
+
+
+let printIt format =
+    Printf.kprintf(fun msg -> printfn "%s" msg) format
+    
 
 [<EntryPoint>]
 let main argv = 
-    let f = Memorization.recursiveMemorize fib
-    let result = f 10
-    printfn "%A" result
+    let log = LogManager.getDefaultLog()
+    log.debug "%A" 1
+
+    printfn "%A" 0
     0 // return an integer exit code
---
+++ CSharp/Source/Arrow/Arrow.FSharp/Logging.fs
@@ -0,0 +1,48 @@
+﻿namespace Arrow.FSharp.Logging
+
+open FSharp.Core
+
+
+type Log = {log : Arrow.Logging.ILog} with
+
+    /// Writes to the debug log
+    member this.debug format =
+        Printf.ksprintf (fun text -> this.log.Debug(text)) format
+
+    /// Writes to the info log
+    member this.info format=
+        Printf.kprintf (fun text -> this.log.Info(text)) format
+
+    /// Writes to the warning log
+    member this.warn format=
+        Printf.kprintf (fun text -> this.log.Warn(text)) format
+
+    /// Writes to the error log
+    member this.error format=
+        Printf.kprintf (fun text -> this.log.Error(text)) format
+
+    /// Writes to the fatal log
+    member this.fatal format=
+        Printf.kprintf (fun text -> this.log.Fatal(text)) format
+
+
+module LogManager =
+    /// Returns the default log
+    let getDefaultLog() =
+        let log = Arrow.Logging.LogManager.GetDefaultLog()
+        {Log.log = log}
+
+    /// Returns the named log
+    let getNamedLog (name : string) =
+        let log = Arrow.Logging.LogManager.GetLog(name)
+        {Log.log = log}
+
+    /// Returns the log for a given type
+    let getLogForType (type' : System.Type) =
+        let log = Arrow.Logging.LogManager.GetLog(type')
+        {Log.log = log}
+
+    /// Returns the log for a given type
+    let getGenericLog<'T> () =
+        let log = Arrow.Logging.LogManager.GetLog<'T>()
+        {Log.log = log}
