a302647080bf8de5b5da1fe306fd0a8e1c623356
Sean Batten <seanbatten58@gmail.com>
2018-03-06 22:43:04 +0000

    Initial implementation of Attempt

--- CSharp/Source/Arrow/Apps/FSharpConsoleApplication/Program.fs
+++ CSharp/Source/Arrow/Apps/FSharpConsoleApplication/Program.fs
@@ -1,19 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open Arrow.FSharp.Logging
+open Arrow.FSharp.Attempt
 
-open FSharp.Core
-
-
-let printIt format =
-    Printf.kprintf(fun msg -> printfn "%s" msg) format
-    
+let divide x y =
+    match y with
+    | 0 -> Failure "divide by zero"
+    | _ -> Success (x / y)
 
 [<EntryPoint>]
 let main argv = 
-    let log = LogManager.getDefaultLog()
-    log.debug "%A" 1
 
-    printfn "%A" 0
+    let x = attempt {
+        try
+            return 10 / 0
+        with
+        | e -> 
+            return! Failure e.Message
+    }
+
+    match x with
+    | Success s -> printfn "Success: %A" s
+    | Failure reason -> printfn "Failure: %A" reason
+    
+    //printfn "%A" 0
     0 // return an integer exit code
---
+++ CSharp/Source/Arrow/Arrow.FSharp/Attempt.fs
@@ -0,0 +1,85 @@
+﻿namespace Arrow.FSharp
+
+module Attempt =
+
+    type Attempt<'T> =
+        | Success of 'T
+        | Failure of string
+    
+    type AttemptBuilder() =
+        member this.Bind(r, f) =
+            match r with
+            | Success s -> 
+                try
+                    f s
+                with
+                    | e -> Failure e.Message
+            | fail -> fail
+
+        member this.Return(x) =
+            Success x
+
+        member this.ReturnFrom(r) =
+            r
+
+        (*
+        member this.Yield(x) =
+            Success x
+
+        member this.YieldFrom(r) =
+            r
+        *)
+
+        member this.Zero() = 
+            Success ()
+
+        member this.Combine(r1, f) =
+            match r1 with
+            | Success _ -> r1
+            | Failure _ -> f()
+
+        member this.Delay(f) =
+            f
+
+        member this.Run(f) =
+            f()
+
+        member this.While(guard, body) =
+            if guard() then
+                //body() |> ignore
+                this.Bind(body(), fun() -> this.While(guard, body))
+            else
+                this.Zero()
+
+        member this.TryWith(body, handler) =
+            try
+                this.ReturnFrom(body())
+            with
+                e -> handler e
+
+        member this.TryFinally(body, compensation) =
+            try
+                this.ReturnFrom(body())
+            finally
+                compensation()
+
+        member this.Using(disposable : #System.IDisposable, body) =
+            try
+                this.ReturnFrom(body(disposable))
+            finally
+                match disposable with
+                | null -> ()
+                | d -> d.Dispose()
+
+        member this.For(sequence : seq<_>, body) =
+            this.Using(sequence.GetEnumerator(), fun enum ->
+                this.While(enum.MoveNext, this.Delay(fun() -> body enum.Current)))
+
+    let attempt = new AttemptBuilder()
+
+    let execute f =
+        try
+            Success (f())
+        with
+        | e -> Failure e.Message
+
