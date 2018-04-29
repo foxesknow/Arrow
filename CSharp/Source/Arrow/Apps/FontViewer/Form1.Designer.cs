a7a7abe4b1ddadb2c961cb66ffbe37ee380aab84
Sean Batten <seanbatten58@gmail.com>
2018-04-29 20:28:04 +0100

    FontViewer app

---
+++ CSharp/Source/Arrow/Apps/FontViewer/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace FontViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lstFonts = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tckFontSize = new System.Windows.Forms.TrackBar();
+            this.txtPreview = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFontSize = new System.Windows.Forms.Label();
+            this.chkItalic = new System.Windows.Forms.CheckBox();
+            this.chkBold = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tckFontSize)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lstFonts);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1038, 668);
+            this.splitContainer1.SplitterDistance = 346;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // lstFonts
+            // 
+            this.lstFonts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstFonts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFonts.FormattingEnabled = true;
+            this.lstFonts.ItemHeight = 16;
+            this.lstFonts.Location = new System.Drawing.Point(0, 0);
+            this.lstFonts.Name = "lstFonts";
+            this.lstFonts.Size = new System.Drawing.Size(346, 668);
+            this.lstFonts.TabIndex = 0;
+            this.lstFonts.SelectedValueChanged += new System.EventHandler(this.lstFonts_SelectedValueChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.tckFontSize, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPreview, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(688, 668);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tckFontSize
+            // 
+            this.tckFontSize.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tckFontSize.Location = new System.Drawing.Point(3, 3);
+            this.tckFontSize.Maximum = 100;
+            this.tckFontSize.Minimum = 1;
+            this.tckFontSize.Name = "tckFontSize";
+            this.tckFontSize.Size = new System.Drawing.Size(682, 34);
+            this.tckFontSize.TabIndex = 0;
+            this.tckFontSize.Value = 10;
+            this.tckFontSize.ValueChanged += new System.EventHandler(this.tckFontSize_ValueChanged);
+            // 
+            // txtPreview
+            // 
+            this.txtPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPreview.Location = new System.Drawing.Point(3, 75);
+            this.txtPreview.Multiline = true;
+            this.txtPreview.Name = "txtPreview";
+            this.txtPreview.Size = new System.Drawing.Size(682, 590);
+            this.txtPreview.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblFontSize);
+            this.panel1.Controls.Add(this.chkItalic);
+            this.panel1.Controls.Add(this.chkBold);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(682, 26);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblFontSize
+            // 
+            this.lblFontSize.AutoSize = true;
+            this.lblFontSize.Location = new System.Drawing.Point(171, 7);
+            this.lblFontSize.Name = "lblFontSize";
+            this.lblFontSize.Size = new System.Drawing.Size(35, 13);
+            this.lblFontSize.TabIndex = 2;
+            this.lblFontSize.Text = "label1";
+            // 
+            // chkItalic
+            // 
+            this.chkItalic.AutoSize = true;
+            this.chkItalic.Location = new System.Drawing.Point(56, 6);
+            this.chkItalic.Name = "chkItalic";
+            this.chkItalic.Size = new System.Drawing.Size(48, 17);
+            this.chkItalic.TabIndex = 1;
+            this.chkItalic.Text = "Italic";
+            this.chkItalic.UseVisualStyleBackColor = true;
+            this.chkItalic.CheckedChanged += new System.EventHandler(this.chkItalic_CheckedChanged);
+            // 
+            // chkBold
+            // 
+            this.chkBold.AutoSize = true;
+            this.chkBold.Location = new System.Drawing.Point(3, 6);
+            this.chkBold.Name = "chkBold";
+            this.chkBold.Size = new System.Drawing.Size(47, 17);
+            this.chkBold.TabIndex = 0;
+            this.chkBold.Text = "Bold";
+            this.chkBold.UseVisualStyleBackColor = true;
+            this.chkBold.CheckedChanged += new System.EventHandler(this.chkBold_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 668);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Font Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tckFontSize)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox lstFonts;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TrackBar tckFontSize;
+        private System.Windows.Forms.TextBox txtPreview;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkItalic;
+        private System.Windows.Forms.CheckBox chkBold;
+        private System.Windows.Forms.Label lblFontSize;
+    }
+}
+
---
+++ CSharp/Source/Arrow/Apps/FontViewer/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FontViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var builder = new StringBuilder();
+
+            for(char c = 'A'; c <= 'Z'; c++)
+            {
+                builder.Append(c).Append(Char.ToLower(c));
+            }
+
+            builder.AppendLine().Append("0123456789");
+            builder.AppendLine().Append("!\"#£$%&'()*+-/\\_,.:;<>=?@[]{}|");
+            builder.AppendLine().AppendLine();
+            builder.Append("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque auctor lectus ac justo vulputate vehicula. Praesent pellentesque, dolor a tempus rhoncus, arcu lorem rhoncus elit, nec tincidunt diam quam et nulla. Cras luctus, ligula imperdiet faucibus congue, odio tellus rhoncus justo, posuere tristique purus metus quis nisi. Vivamus eget dapibus metus, et scelerisque libero. In hac habitasse platea dictumst. Aenean vel mauris a turpis euismod venenatis. Morbi eu commodo nibh, vitae vestibulum risus.");
+
+            txtPreview.Text = builder.ToString();
+
+            foreach(FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                var info = new FontInfo()
+                {
+                    Family = font
+                };
+
+                int index = lstFonts.Items.Add(info);
+            }
+
+            lstFonts.SelectedIndex = 0;
+        }
+
+        private void lstFonts_SelectedValueChanged(object sender, EventArgs e)
+        {
+            UpdatePreview();
+        }
+
+        private void UpdatePreview()
+        {
+            lblFontSize.Text = string.Format("Font Size : {0}", tckFontSize.Value);
+
+            var fontInfo = lstFonts.SelectedItem as FontInfo;
+            
+            if(fontInfo != null)
+            {
+                FontStyle style = FontStyle.Regular;
+                if(chkBold.Checked) style |= FontStyle.Bold;
+                if(chkItalic.Checked) style |= FontStyle.Italic;
+
+                txtPreview.Font = new Font(fontInfo.Family, tckFontSize.Value, style);
+            }
+        }
+
+        private void tckFontSize_ValueChanged(object sender, EventArgs e)
+        {           
+            UpdatePreview();
+        }
+
+        private void chkItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdatePreview();
+        }
+
+        private void chkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdatePreview();
+        }
+    }
+
+    class FontInfo
+        {
+            public FontFamily Family;
+
+            public override string ToString()
+            {
+                return Family.Name;
+            }
+        }
+}
