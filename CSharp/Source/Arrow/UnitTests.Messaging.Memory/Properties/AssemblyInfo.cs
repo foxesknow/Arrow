c357b805cf9fbbf8778ef3815f8babb5801b3d11
Sean <seanbatten58@gmail.com>
2022-10-13 18:00:21 +0100

    Moved unit tests into solution

---
+++ CSharp/Source/Arrow/UnitTests.Messaging.Memory/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d4d4226-ec5c-4f19-8f6e-b5b48e87db7d")]
