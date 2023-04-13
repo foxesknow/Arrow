64595c368b63b6d4af91554af9786e8d1a07f338
Sean <seanbatten58@gmail.com>
2023-04-13 16:17:43 +0100

    Added RequireX classes

---
+++ CSharp/Source/Arrow/Arrow/Execution/RequireStruct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.Execution
+{
+    /// <summary>
+    /// Allows you to overload a method which take a generic parameter
+    /// that name the same number of parameters but a generic contraint
+    /// restricting the type to be a class of struct
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <example>
+    /// Instead of
+    /// <![CDATA[
+    /// void Foo<T>(T value) where T : struct{}
+    /// void Foo<T>(T value) where T : class{}
+    /// 
+    /// which won't compile, use:
+    /// 
+    /// void Foo<T>(T value, RequireStruct<T>? _ = null) where T : struct{}
+    /// void Foo<T>(T value) where T : class{}
+    /// ]]>
+    /// </example>
+    public sealed class RequireStruct<T> where T : class
+    {
+        private RequireStruct()
+        {
+        }
+    }
+}
