f88386780e05b525300ad66a8a1c227c20a3521f
Sean <seanbatten58@gmail.com>
2023-04-13 14:43:13 +0100

    Added QuickSettings

---
+++ CSharp/Source/Arrow/Arrow/Settings/QuickSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.Settings
+{
+    /// <summary>
+    /// Provides a quick way to create settings in an application and
+    /// progmatically register them
+    /// </summary>
+    public sealed class QuickSettings : ISettings
+    {
+        private readonly IReadOnlyDictionary<string, object> m_Settings;
+
+        /// <summary>
+        /// Initializes the instance
+        /// </summary>
+        /// <param name="settings"></param>
+        public QuickSettings(params (string Name, object value)[] settings)
+            :this((IEnumerable<(string Name, object value)>)settings)
+        {
+        }
+
+        /// <summary>
+        /// Initializes the instance
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public QuickSettings(IEnumerable<(string Name, object value)> settings)
+        {
+            if(settings is null) throw new ArgumentNullException(nameof(settings));
+
+            m_Settings = settings.ToDictionary(pair => pair.Name, pair => pair.value, StringComparer.OrdinalIgnoreCase);
+        }
+
+        /// <inheritdoc/>
+        public bool TryGetSetting(string name, [NotNullWhen(true)] out object? value)
+        {
+            return m_Settings.TryGetValue(name, out value);
+        }
+    }
+}
---
+++ CSharp/Source/Arrow/UnitTests.Arrow/Settings/QuickSettingsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Arrow.Settings;
+
+using NUnit.Framework;
+
+namespace UnitTests.Arrow.Settings
+{
+    [TestFixture]
+    public class QuickSettingsTests
+    {
+        [Test]
+        public void Initialization()
+        {
+            var settings = new QuickSettings(("Name", "Jack"), ("Age", 42));
+
+            Assert.That(settings.TryGetSetting("name", out var name), Is.True);
+            Assert.That(name, Is.EqualTo("Jack"));
+
+            Assert.That(settings.TryGetSetting("age", out var age), Is.True);
+            Assert.That(age, Is.EqualTo(42));
+
+            Assert.That(settings.TryGetSetting("location", out var location), Is.False);
+            Assert.That(location, Is.Null);
+        }
+    }
+}
