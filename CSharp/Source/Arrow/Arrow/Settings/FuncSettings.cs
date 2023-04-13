043920ecfc9620a0835fc9009f610aa20581eeb1
Sean <seanbatten58@gmail.com>
2023-04-13 15:56:40 +0100

    FuncSettings

---
+++ CSharp/Source/Arrow/Arrow/Settings/FuncSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.Settings
+{
+    /// <summary>
+    /// Looks up a setting by calling a function
+    /// </summary>
+    public sealed class FuncSettings : ISettings
+    {
+        private readonly Func<string, object?> m_Lookup;
+
+        /// <summary>
+        /// Initializes the instance
+        /// </summary>
+        /// <param name="lookup"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public FuncSettings(Func<string, object?> lookup)
+        {
+            if(lookup is null) throw new ArgumentNullException(nameof(lookup));
+
+            m_Lookup = lookup;
+        }
+        
+        /// <inheritdoc/>
+        public bool TryGetSetting(string name, [NotNullWhen(true)] out object? value)
+        {
+            value = m_Lookup(name);
+            return value is not null;
+        }
+    }
+}
---
+++ CSharp/Source/Arrow/UnitTests.Arrow/Settings/FuncSettingsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Arrow.Settings;
+
+using NUnit.Framework;
+
+namespace UnitTests.Arrow.Settings
+{
+    [TestFixture]
+    public class FuncSettingsTests
+    {
+        [Test]
+        public void Lookup()
+        {
+            var settings = new FuncSettings(GetSettings);
+
+            Assert.That(settings.TryGetSetting("Name", out var name), Is.True);
+            Assert.That(name, Is.EqualTo("Jack"));
+
+            Assert.That(settings.TryGetSetting("Location", out var location), Is.False);
+            Assert.That(location, Is.Null);
+        }
+
+        private static object? GetSettings(string name)
+        {
+            return name.ToLower() switch
+            {
+                "name"  => "Jack",
+                "age"   => 42,
+                _       => null
+            };
+        }
+    }
+}
