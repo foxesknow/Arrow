93f94585af652aeef8bcc85f7167d155ac96d93e
Sean <seanbatten58@gmail.com>
2023-04-14 15:55:44 +0100

    Postgres support

---
+++ CSharp/Source/Arrow/Arrow.Postgres/PostgresDatabaseDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using Arrow.Data;
+
+using Npgsql;
+
+namespace Arrow.Postgres
+{
+    public sealed class PostgresDatabaseDetails : DatabaseDetails
+    {
+        /// <inheritdoc/>
+        public override IDbConnection CreateConnection()
+        {
+            if(this.ConnectionString == null) throw new ArgumentNullException(nameof(ConnectionString));
+
+            var connection = new NpgsqlConnection(this.ConnectionString);
+            return connection;
+        }
+
+        /// <summary>
+        /// The Postgres connection string
+        /// </summary>
+        public string? ConnectionString{get; set;}
+    }
+}
