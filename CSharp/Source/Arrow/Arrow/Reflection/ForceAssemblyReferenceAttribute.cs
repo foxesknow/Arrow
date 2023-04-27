64bca51751c1d21f177b2ad50f97f851faf6a64e
Sean <seanbatten58@gmail.com>
2023-04-27 12:33:34 +0100

    ForceAssemblyReferenceAttribute

---
+++ CSharp/Source/Arrow/Arrow/Reflection/ForceAssemblyReferenceAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.Reflection
+{
+    /// <summary>
+    /// Allows you to force a reference to assembly that may not be used in your code
+    /// but your application will dynamically load, adn so you don't want the compiler
+    /// to be clever and not copy it to the output directory
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public sealed class ForceAssemblyReferenceAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes the instance
+        /// </summary>
+        /// <param name="type">A type from an assembly we want to force a reference to</param>
+        public ForceAssemblyReferenceAttribute(Type type)
+        {
+            this.Type = type;
+        }
+
+        /// <summary>
+        /// The type we used to force the reference
+        /// </summary>
+        public Type Type{get;}
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return this.Type.ToString();
+        }
+    }
+}
