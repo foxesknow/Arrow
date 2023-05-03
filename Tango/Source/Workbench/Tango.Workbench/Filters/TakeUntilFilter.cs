b4793fa0299064455520cf368e6ce2371f17a43b
Sean <seanbatten58@gmail.com>
2023-05-03 09:18:26 +0100

    Take filters

---
+++ Tango/Source/Workbench/Tango.Workbench/Filters/TakeUntilFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango.Workbench.Filters
+{
+    [Filter("TakeUntil")]
+    public sealed class TakeUntilFilter : PredicateFilterBase
+    {
+        public override async IAsyncEnumerable<object> Run(IAsyncEnumerable<object> items)
+        {
+            if(this.Predicate is null) throw new ArgumentNullException(nameof(Predicate));
+
+            long index = 0;
+            
+            await foreach(var item in items)
+            {
+                var itemType = item.GetType();
+                var predicate = GetPredicate(this.Predicate, itemType);
+
+                if(predicate(item, index++))
+                {
+                    yield break;
+                }
+                else
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// While the predicate becomes true we will stop return items
+        /// </summary>
+        public string? Predicate{get; set;}
+    }
+}
---
+++ Tango/Source/Workbench/Tango.Workbench/Filters/TakeWhileFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango.Workbench.Filters
+{
+    [Filter("TakeWhile")]
+    public sealed class TakeWhileFilter : PredicateFilterBase
+    {
+        public override async IAsyncEnumerable<object> Run(IAsyncEnumerable<object> items)
+        {
+            if(this.Predicate is null) throw new ArgumentNullException(nameof(Predicate));
+
+            long index = 0;
+            
+            await foreach(var item in items)
+            {
+                var itemType = item.GetType();
+                var predicate = GetPredicate(this.Predicate, itemType);
+
+                if(predicate(item, index++))
+                {
+                    yield return item;
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// While the predicate is true we will return items
+        /// </summary>
+        public string? Predicate{get; set;}
+    }
+}
