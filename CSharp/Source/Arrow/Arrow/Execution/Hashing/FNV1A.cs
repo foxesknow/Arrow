cf63ba01549fe90a52edea5850f9048e998f3f31
foxesknow <seanbatten58@gmail.com>
2024-09-08 17:33:54 +0100

    Added FNV1a

---
+++ CSharp/Source/Arrow/Arrow/Execution/Hashing/FNV1A.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrow.Execution.Hashing
+{
+    /// <summary>
+    /// An implementation of the Fowler Noll Vo hash function (https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function)
+    /// This hashing algorithm is fast and provides reasonbly good hash coverage.
+    /// Types larger than a byte are converted to a little endian array and the contents of the array is hashed.
+    /// 
+    /// NOTE: This is a mutable struct. If you need to pass it to other methods then pass it by reference, not by value.
+    /// </summary>
+    public ref struct FNV1A
+    {
+        private const uint OffsetBasis = 0x811c9dc5U;
+        private const uint Prime = 0x01000193U;
+
+        private uint m_Hash;
+
+        /// <summary>
+        /// Prepares the struct for hashing
+        /// </summary>
+        public void Begin()
+        {
+            m_Hash = OffsetBasis;
+        }
+
+        /// <summary>
+        /// Returns the current hash value
+        /// </summary>
+        public readonly int HashValue
+        {
+            get{return unchecked((int)m_Hash);}
+        }
+
+        /// <inheritdoc/>
+        public readonly override int GetHashCode()
+        {
+            return unchecked((int)m_Hash);
+        }
+
+        /// <summary>
+        /// Applies a byte to the hasher
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The current hash value</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Apply(byte value)
+        {
+            unchecked
+            {
+                m_Hash ^= value;
+                m_Hash *= Prime;
+
+                return (int)m_Hash;
+            }
+        }
+
+        /// <summary>
+        /// Applies a short to the hasher
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The current hash value</returns>
+        public int Apply(short value)
+        {
+            Span<byte> buffer = stackalloc byte[sizeof(short)];
+            BinaryPrimitives.WriteInt16LittleEndian(buffer, value);
+            return Apply(buffer);
+        }
+
+        /// <summary>
+        /// Applies an int to the hasher
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The current hash value</returns>
+        public int Apply(int value)
+        {
+            Span<byte> buffer = stackalloc byte[sizeof(int)];
+            BinaryPrimitives.WriteInt32LittleEndian(buffer, value);
+            return Apply(buffer);
+        }
+
+        /// <summary>
+        /// Applies a long to the hasher
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The current hash value</returns>
+        public int Apply(long value)
+        {
+            Span<byte> buffer = stackalloc byte[sizeof(long)];
+            BinaryPrimitives.WriteInt64LittleEndian(buffer, value);
+            return Apply(buffer);
+        }
+
+        /// <summary>
+        /// Applies a float to the hasher
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The current hash value</returns>
+        public int Apply(float value)
+        {
+            Span<byte> buffer = stackalloc byte[sizeof(float)];
+            BinaryPrimitives.WriteSingleLittleEndian(buffer, value);
+            return Apply(buffer);
+        }
+
+        /// <summary>
+        /// Applies a double to the hasher
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The current hash value</returns>
+        public int Apply(double value)
+        {
+            Span<byte> buffer = stackalloc byte[sizeof(double)];
+            BinaryPrimitives.WriteDoubleLittleEndian(buffer, value);
+            return Apply(buffer);
+        }
+
+        /// <summary>
+        /// Applies a sequence of bytes to the hasher
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns>The current hash value</returns>
+        public int Apply(scoped ReadOnlySpan<byte> values)
+        {
+            unchecked
+            {
+                var length = values.Length;
+
+                for(int i = 0; i < length; i++)
+                {
+                    var value = values[i];
+                    m_Hash ^= value;
+                    m_Hash *= Prime;
+                }
+
+                return (int)m_Hash;
+            }
+        } 
+
+        /// <summary>
+        /// Creates a FNV1a hasher that has had Begin() called on it
+        /// </summary>
+        /// <returns></returns>
+        public static FNV1A Make()
+        {
+            var hasher = new FNV1A();
+            hasher.Begin();
+
+            return hasher;
+        }
+    }
+}
---
+++ CSharp/Source/Arrow/UnitTests.Arrow/Execution/Hashing/FNV1ATests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Arrow.Execution.Hashing;
+
+using NUnit.Framework;
+
+namespace UnitTests.Arrow.Execution.Hashing
+{
+    [TestFixture]
+    public class FNV1ATests
+    {
+        [Test]
+        public void DefaultInitialization()
+        {
+            FNV1A hasher = default;
+            Assert.That(hasher.GetHashCode(), Is.EqualTo(0));
+            Assert.That(hasher.HashValue, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Begin()
+        {
+            // This is the FNV1a offset basis for 32bit hashing
+            var offsetBasis = unchecked((int)0x811c9dc5);
+
+            FNV1A hasher = new();
+            hasher.Begin();
+            Assert.That(hasher.GetHashCode(), Is.EqualTo(offsetBasis));
+        }
+
+        [Test]
+        public void End()
+        {
+            // This is the FNV1a offset basis for 32bit hashing
+            var offsetBasis = unchecked((int)0x811c9dc5);
+
+            FNV1A hasher = new();
+            hasher.Begin();
+            Assert.That(hasher.HashValue, Is.EqualTo(offsetBasis));
+        }
+
+        [Test]
+        public void HashesAreEqual_IntegerTypes()
+        {
+            var random = new Random();
+
+            for(int i = 0; i < 100; i ++)
+            {
+                var value = random.Next(int.MinValue, int.MaxValue);
+
+                var hasher1 = FNV1A.Make();
+                hasher1.Begin();
+                hasher1.Apply((byte)value);
+                hasher1.Apply((short)value);
+                hasher1.Apply((int)value);
+                hasher1.Apply((long)value);
+
+                var hasher2 = FNV1A.Make();
+                hasher2.Begin();
+                hasher2.Apply((byte)value);
+                hasher2.Apply((short)value);
+                hasher2.Apply((int)value);
+                hasher2.Apply((long)value);
+
+                Assert.That(hasher1.HashValue, Is.EqualTo(hasher2.HashValue));
+            }
+        }
+
+        [Test]
+        public void HashesAreEqual_FloatTypes()
+        {
+            var random = new Random();
+
+            for(int i = 0; i < 100; i ++)
+            {
+                var doubleValue = random.NextDouble() * 10_000;
+                var floatValue = random.NextDouble() * 10_000;
+
+                var hasher1 = FNV1A.Make();
+                hasher1.Begin();
+                hasher1.Apply(doubleValue);
+                hasher1.Apply(floatValue);
+
+                var hasher2 = FNV1A.Make();
+                hasher2.Begin();
+                hasher2.Apply(doubleValue);
+                hasher2.Apply(floatValue);
+
+                Assert.That(hasher1.HashValue, Is.EqualTo(hasher2.HashValue));
+            }
+        }
+    }
+}
